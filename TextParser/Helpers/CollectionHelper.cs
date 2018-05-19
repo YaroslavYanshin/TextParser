34b324c9a465084942056b87d898cad4772fc47b
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2018-05-19 13:20:40 +0300

    Add class CollectionHelpers

---
+++ TextParser/Helpers/CollectionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextParser.Helpers
+{
+    public static class CollectionHelper
+    {
+        public static void AddRange<T>(this ICollection<T> destination, IEnumerable<T> source)
+        {
+            foreach (var item in source)
+            {
+                destination.Add(item);
+            }
+        }
+    }
+}
